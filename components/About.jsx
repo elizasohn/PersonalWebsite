775e2a811c1698746c5a542cd343d9bd5f62cb75
Eliza Sohn <eliza.sohn@gmail.com>
2022-08-10 01:39:01 -0700

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -1,14 +1,14 @@
-function About() {
+import React from 'react';
+
+export default function About() {
     return (
-        <div class="container">
+        <div className="container">
             <img
-                class="img" 
-                src="../images/eliza.jpg" 
+                className="img" 
+                src={require("../assets/muchfiner.jpeg")}
                 alt="Eliza djing with some datamoshing photoshopped in"
             />
             <p>Hi! My name is Eliza Sohn, and this is my introduction. I'm currently a graduate student at Portland State University in Computer Science. I have a BFA in Intermedia, and I love playing soccer.</p>
         </div>
     )
 };
-
-export default About;
