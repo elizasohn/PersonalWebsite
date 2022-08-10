f194cf8d1bbc941d05950d2dd77f2e1025fa3c21
Eliza Sohn <eliza.sohn@gmail.com>
2022-08-10 00:33:18 -0700

    add components to App

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
 import Resume from './components/Resume';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <About/>
+      <Resume/>
+      <Projects/>
+      <Contact/>
     </div>
   );
 }
