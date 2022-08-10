ae7762b08814ae56a29dbb7dd591b770a84f4e9e
Eliza Sohn <eliza.sohn@gmail.com>
2022-08-10 04:12:22 -0700

    Update Contact.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,7 +1,24 @@
 function Contact() {
     return (
-        <div class="container">
-            <h1>Contact Meee</h1>
+        <div className="container">
+           <form className="form" action="">
+                <div className="form-control">
+                    <label >Name:</label>
+                    <input id="name" type="name" name="name"></input>
+                </div>
+                <div className="form-control">
+                    <label>Email:</label>
+                    <input id="email" type="email" name="email"></input>
+                </div>
+                <div className="form-control">
+                    <label for="email">Query:</label>
+                    <textarea id="query" type="query" name="query"></textarea>
+                </div>
+                <div className="btn">
+                    <input className="btn-primary" type="submit" name="submit" value="Submit"/>
+                    <input className="btn-secondary" type="reset" name="reset" value="Reset"/>
+                </div>
+            </form>
         </div>
     )
 };
