b8f527a739c90e68f168723f9e921edc8c5fcf62
Eliza Sohn <eliza.sohn@gmail.com>
2022-08-10 01:38:48 -0700

    Update App.js

--- App.js
+++ App.js
@@ -4,15 +4,18 @@ import About from './components/About';
 import Resume from './components/Resume';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
-      <About/>
-      <Resume/>
-      <Projects/>
-      <Contact/>
+      <Routes>
+        <Route exact path='/' element={<About/>}/>
+        <Route exact path='/projects' element={<Projects/>}/>
+        <Route exact path='/resume' element={<Resume/>}/>
+        <Route exact path='/contact' element={<Contact/>}/>
+      </Routes>
     </div>
   );
 }
