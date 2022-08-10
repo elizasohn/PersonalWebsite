5b640696e338bfea076a0a2ff322af7447a0deef
Eliza Sohn <eliza.sohn@gmail.com>
2022-08-10 00:31:17 -0700

    set up initial components

---
+++ personal-website/src/components/Contact.jsx
@@ -0,0 +1,9 @@
+function About() {
+    return (
+        <div class="container">
+            <h1>Contact Meee</h1>
+        </div>
+    )
+};
+
+export default About;
---
+++ personal-website/src/components/Nav.jsx
@@ -0,0 +1,25 @@
+function Nav() {
+    return (
+        <nav>
+            <ul class="nav-list">
+                <li class="list-item">
+                    <a class="nav-link" href="/hw1/01-hello.html" alt="link to first exercise, 01-hello.html">Exercise 01</a>
+                </li>
+                <li class="list-item">
+                    <a class="nav-link" href="/hw1/02-card.html" alt="link to second exercise, 02-card.html">Exercise 02</a>
+                </li>
+                <li class="list-item">
+                    <a class="nav-link" href="/hw1/03-navbar.html" alt="link to third exercise, 03-navbar.html">Exercise 03</a>
+                </li>
+                <li class="list-item">
+                    <a class="nav-link" href="/hw1/04-table.html" alt="link to fourth exercise, 04-table.html">Exercise 04</a>
+                </li>
+                <li class="list-item">
+                    <a class="nav-link" href="/hw1/05-form.html" alt="link to fifth exercise, 05-form.html">Exercise 05</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
