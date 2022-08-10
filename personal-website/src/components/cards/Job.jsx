ced44a8d5510ccdb11fab875047c415917b3733c
Eliza Sohn <eliza.sohn@gmail.com>
2022-08-10 16:21:22 -0700

    update formatting

--- personal-website/src/components/cards/Job.jsx
+++ personal-website/src/components/cards/Job.jsx
@@ -1,10 +1,15 @@
 function Job(props){
     return (
         <div className='job-card'>
-            <h3 className='job-time'>{props.jobTime}</h3>
-            <h1 className='job-title'>{props.jobTitle}</h1>
-            <h2 className='job-company'>{props.jobCompany}</h2>
-            <p className='job-description'>{props.jobDescription}</p>
+            <div className='job-info-left'>
+                <h3 className='job-time'>{props.jobTime}</h3>
+                <h1 className='job-title'>{props.jobTitle}</h1>
+                <h2 className='job-company'>{props.jobCompany}</h2>
+                <h2 className='job-location'>{props.jobLocation}</h2>
+            </div>
+            <div className='job-info-right'>
+                <p className='job-description'>{props.jobDescription}</p>
+            </div>
         </div>
     )
 }
