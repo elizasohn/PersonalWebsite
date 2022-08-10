289d952f3bf138ac0ad4be1bbd6e34d474c6b510
Eliza Sohn <eliza.sohn@gmail.com>
2022-08-10 16:21:14 -0700

    update formatting

--- components/About.jsx
+++ components/About.jsx
@@ -3,13 +3,21 @@ import React from 'react';
 export default function About() {
     return (
         <div className="container">
+            <h1 className='greeting'>Hi, I'm Eliza</h1>
             <div className="about">
-            <img
-                className="img" 
-                src={require("../assets/muchfiner.jpeg")}
-                alt="Eliza djing with some datamoshing photoshopped in"
-            />
-            <p>Hi, I'm Eliza (she/her). I'm a creative, self-motivated programmer. I'm passionate about problem-solving and love making things. I currently work as a developer at The Good, and am a graduate student studying Computer Science at Portland State University. I have a background in Fine Arts with a BFA from Pacific Northwest College of Art. You can find me hosting the show Much Finer on Xray.fm where I play mostly vinyl, or playing soccer in my free time. </p>
+                <div className="portrait">
+                    <img
+                        className="img" 
+                        src={require("../assets/muchfiner.jpeg")}
+                        alt="Eliza djing with some datamoshing photoshopped in"
+                    />
+                </div>
+                <div className='description'>
+                    <p>I'm a creative, self-motivated programmer. I'm passionate about problem-solving and love making things.</p>
+                    <p>I currently work as a developer at The Good, and am a graduate student studying Computer Science at Portland State University. </p>
+                    <p>I have a background in Fine Arts with a BFA from Pacific Northwest College of Art. </p>
+                    <p>You can find me hosting the show <a href='https://xray.fm/shows/much-finer'>Much Finer on Xray.fm </a>where I play mostly vinyl, or playing soccer in my free time. </p>
+                </div>
             </div>
         </div>
     )
