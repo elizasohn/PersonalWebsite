f29cd0823520cc552f52b871da45995ac3cb1629
Eliza Sohn <eliza.sohn@gmail.com>
2022-08-10 04:12:15 -0700

    add longer description

--- personal-website/src/components/About.jsx
+++ personal-website/src/components/About.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 export default function About() {
     return (
         <div className="container">
+            <div className="about">
             <img
                 className="img" 
                 src={require("../assets/muchfiner.jpeg")}
                 alt="Eliza djing with some datamoshing photoshopped in"
             />
-            <p>Hi! My name is Eliza Sohn, and this is my introduction. I'm currently a graduate student at Portland State University in Computer Science. I have a BFA in Intermedia, and I love playing soccer.</p>
+            <p>Hi, I'm Eliza (she/her). I'm a creative, self-motivated programmer. I'm passionate about problem-solving and love making things. I currently work as a developer at The Good, and am a graduate student studying Computer Science at Portland State University. I have a background in Fine Arts with a BFA from Pacific Northwest College of Art. You can find me hosting the show Much Finer on Xray.fm where I play mostly vinyl, or playing soccer in my free time. </p>
+            </div>
         </div>
     )
 };
